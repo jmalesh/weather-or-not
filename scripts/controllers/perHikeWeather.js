cc7f6e592b88738289f1801bf67896547d762a1b
Jeffrey Gebhardt <jeff@gebbey.com>
2016-05-31 10:59:52 -0700

    marker click event

---
+++ scripts/controllers/perHikeWeather.js
@@ -0,0 +1,15 @@
+var getHikeWeatherForecast = function(hikeLat, hikeLng) {
+  'use strict';
+
+  var weatherUrl = 'http://api.wunderground.com/api/4cad78aa1aa5f7ef/forecast/q/' + hikeLat + ',' + hikeLng + '.json';
+
+  console.log(weatherUrl);
+
+  $.ajax({
+    url: weatherUrl,
+    dataType: 'jsonp',
+    success:function(data){
+      console.log(data);
+    }
+  });
+};
