ce569f0c78e9cb22c9d3450600bb4913c53a8767
Jeffrey Gebhardt <jeff@gebbey.com>
2016-06-01 14:24:29 -0700

    map customization and bug fixing

--- scripts/controllers/mapController.js
+++ scripts/controllers/mapController.js
@@ -2,8 +2,12 @@
   var mapController = {};
 
   mapController.index = function() {
-    $('#landing-info').hide();
-    $('#map-section').fadeIn();
+    $('#map-section').fadeIn(function(){
+      google.maps.event.trigger(map, 'resize');
+      var center = new google.maps.LatLng(47, -120);
+      map.setCenter(center);
+      map.setZoom(7);
+    }).siblings().hide();
   };
 
   module.mapController = mapController;
